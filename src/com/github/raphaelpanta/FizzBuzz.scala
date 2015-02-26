197747300eb972fe98cc92c339c2947274a5466e
unknown <rpspantaleao@GERTEC326.sefaz.se.gov.br>
2015-02-26 15:57:48 -0300

    FizzBuzz Specification: succesful case, divisible by 3 and 5 must be equal to FizzBuzz

--- src/com/github/raphaelpanta/FizzBuzz.scala
+++ src/com/github/raphaelpanta/FizzBuzz.scala
@@ -3,6 +3,7 @@ package com.github.raphaelpanta
 class FizzBuzz {
 
   def fizzBuzz(number: Int): String = number match {
+    case number if number % 15 == 0 => "FizzBuzz"
     case number if number % 3 == 0 => "Fizz"
     case number if number % 5 == 0 => "Buzz"
   }
