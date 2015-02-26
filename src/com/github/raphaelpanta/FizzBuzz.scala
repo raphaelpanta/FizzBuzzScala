58e04118d977efbc013b10b16f571e6aab6b5fe3
unknown <rpspantaleao@GERTEC326.sefaz.se.gov.br>
2015-02-26 15:50:13 -0300

    FizzBuzz Specification: second succesful case, 5 must be equal to Buzz

--- src/com/github/raphaelpanta/FizzBuzz.scala
+++ src/com/github/raphaelpanta/FizzBuzz.scala
@@ -2,7 +2,8 @@ package com.github.raphaelpanta
 
 class FizzBuzz {
 
-  def fizzBuzz(number: Int): String = {
-    return "Fizz"
+  def fizzBuzz(number: Int): String = number match {
+    case 3 => "Fizz"
+    case 5 => "Buzz"
   }
 }
